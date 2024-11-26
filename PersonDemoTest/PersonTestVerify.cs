8ef3e150eaa5e1126c9fa3ea568965c5dffa02a9
Lucas Derksen <luc.derksen@gmail.com>
2024-11-26 19:48:58 +0100

    test

--- PersonDemoTest/PersonTestVerify.cs
+++ PersonDemoTest/PersonTestVerify.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using PersonDemo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PersonDemoTest
+
 {
-    internal class PersonTestVerify
+    public class PersonTestVerify
     {
+        [Fact]
+        public Task PersonTest() 
+        {
+            //Arrange
+            string fullName = "Piet Jansen";
+            int age = 35;
+            string licensePlate = "BB-111-A";
+
+            //Act
+            Taxidriver createdPerson = new Taxidriver(fullName, age, licensePlate);
+
+            //Assert
+            return Verify(createdPerson);
+        }
     }
 }
